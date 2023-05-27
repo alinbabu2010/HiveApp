e9d0fb6de7e6d095cf3e6cee5d278c75f4d40291
Alin Babu <alinbabu2010@outlook.com>
2023-05-27 22:54:44 +0530

    Add home screen in main.dart also disable debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive_app/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Placeholder(),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }

19139fe91790c7ca6788fcd7c547d28af0ef3503
Alin Babu <alinbabu2010@outlook.com>
2023-05-28 15:58:04 +0530

    Refactor code and fix warnings

--- lib/data/source/database_source.dart
+++ lib/data/source/database_source.dart
@@ -6,34 +6,36 @@ import '../model/data_model.dart';
 class DatabaseSource {
   ValueNotifier<List<DataModel>> dataListNotifier = ValueNotifier([]);
 
+  Future<Box<DataModel>> get _database => Hive.openBox<DataModel>('app_db');
+
   Future<void> saveData(DataModel model, dynamic key) async {
     if (key == null) {
-      addData(model);
+      _addData(model);
       return;
     }
     _updateData(model, key);
     getAllData();
   }
 
-  Future<void> addData(DataModel model) async {
-    final database = await Hive.openBox<DataModel>('app_db');
-    await database.add(model);
-    getAllData();
-  }
-
   Future<void> getAllData() async {
-    final database = await Hive.openBox<DataModel>('app_db');
+    final database = await _database;
     dataListNotifier.value = database.values.toList();
   }
 
   Future<void> deleteData(int id) async {
-    final database = await Hive.openBox<DataModel>('app_db');
+    final database = await _database;
     database.delete(id);
     getAllData();
   }
 
+  Future<void> _addData(DataModel model) async {
+    final database = await _database;
+    await database.add(model);
+    getAllData();
+  }
+
   Future<void> _updateData(DataModel model, dynamic key) async {
-    final database = await Hive.openBox<DataModel>('app_db');
+    final database = await _database;
     database.put(key, model);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }

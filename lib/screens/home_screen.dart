15c1018cdb9c80c8ba70823a5d559b1b401729db
Alin Babu <alinbabu2010@outlook.com>
2023-05-28 13:54:52 +0530

    Add database getAllData method invoke on home screen loading

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,6 +11,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    databaseSource.getAllData();
     return Scaffold(
       resizeToAvoidBottomInset: true,
       body: SafeArea(

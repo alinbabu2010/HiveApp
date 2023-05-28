f82b09254b18dd3330bcfeda9967d5062fa1861d
Alin Babu <alinbabu2010@outlook.com>
2023-05-28 15:47:34 +0530

    Add method to update data and public method for add and update data

--- lib/data/source/database_source.dart
+++ lib/data/source/database_source.dart
@@ -6,6 +6,15 @@ import '../model/data_model.dart';
 class DatabaseSource {
   ValueNotifier<List<DataModel>> dataListNotifier = ValueNotifier([]);
 
+  Future<void> saveData(DataModel model, dynamic key) async {
+    if (key == null) {
+      addData(model);
+      return;
+    }
+    _updateData(model, key);
+    getAllData();
+  }
+
   Future<void> addData(DataModel model) async {
     final database = await Hive.openBox<DataModel>('app_db');
     await database.add(model);
@@ -21,5 +30,10 @@ class DatabaseSource {
     final database = await Hive.openBox<DataModel>('app_db');
     database.delete(id);
     getAllData();
+  }
+
+  Future<void> _updateData(DataModel model, dynamic key) async {
+    final database = await Hive.openBox<DataModel>('app_db');
+    database.put(key, model);
   }
 }
